7ff89879740e584f24744df76f0787f55c6aefaa
Adreboot <courgey.adrien@gmail.com>
2024-05-16 11:28:22 +0200

    Add "Add task" functionality

--- js/main.js
+++ js/main.js
@@ -9,3 +9,21 @@ function changeTheme(){
     document.querySelector(".todo")?.classList.toggle("dark-todo");
 }
 
+function addTask(taskName){
+    const template = document.getElementById("task");
+    const taskToAdd = template.content.cloneNode(true);
+    const taskLi = taskToAdd.querySelector("li");
+
+    taskLi.innerText= taskName;
+
+    const taskList = document.querySelector(".todo-list");
+    taskList.appendChild(taskToAdd);
+}
+
+const addTaskButton = document.querySelector(".todo-btn");
+
+addTaskButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    const textInput = document.querySelector("form input");
+    addTask(textInput.value);
+})
